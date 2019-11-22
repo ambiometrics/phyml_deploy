14762bde1a9ff956ffdaf12db55751dd4f01d946
Edwin Rodríguez <edwrodrig@gmail.com>
2019-11-22 16:12:59 -0300

    First linux commit

---
+++ 01.prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#This instructions are based on the README at following location https://github.com/stephaneguindon/phyml
+
+rm -rf sources
+mkdir sources && cd sources
+
+#Download the latest releasea from https://github.com/stephaneguindon/phyml/releases
+wget -O phyml.tar.gz https://github.com/stephaneguindon/phyml/archive/v3.3.20190909.tar.gz
+
+tar xfvz phyml.tar.gz
+
+#prepare directory
+cd phyml-3.3.20190909
+
+sh ./autogen.sh;
+./configure --enable-phyml
+make
+
+rm -rf ../../release
+mkdir ../../release
+cp src/phyml ../../release/
+
+
