11b65ea1acf9d8f2f8bf7a6c1b3fe813749fb7f5
Edwin Rodriguez <edwrodrig@gmail.com>
2019-11-26 16:30:50 -0300

    Generating windows binary in linux

--- 01.prepare.sh
+++ 01.prepare.sh
@@ -13,11 +13,11 @@ tar xfvz phyml.tar.gz
 cd phyml-3.3.20190909
 
 sh ./autogen.sh;
-./configure --enable-phyml
+ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ./configure --enable-win --enable-phyml --host=x86_64-w64-mingw32
 make
 
 rm -rf ../../release
 mkdir ../../release
-cp src/phyml ../../release/
+cp src/phyml-windows.exe ../../release/phyml.exe
 
 
